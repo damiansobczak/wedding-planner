ab790afe211e20bd0047aab237bf236e9a79e633
damiansobczak <sobczakdamianpl@gmail.com>
2021-10-27 17:59:21 +0200

    first commit

---
+++ resources/lang/pl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Podane dane nie są poprawne.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Przekroczono ilość dozwolonych prób. Spróbuj ponownie za :seconds sekund.',
+
+];
